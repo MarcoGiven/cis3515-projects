87a98eae2e7029fc622bde9eef1cb3eb8c73c22e
MarcoGiven <marcogiven15@gmail.com>
2026-01-27 11:57:23 -0500

    added 2 buttons and checkbox to layout & declared in MainActivity.kt

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,39 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var decreaseButton: Button
+    lateinit var increaseButton: Button
+    lateinit var textView: TextView
+    lateinit var checkBox: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        decreaseButton = findViewById(R.id.buttonDecrease)
+        increaseButton = findViewById(R.id.buttonIncrease)
+        textView = findViewById(R.id.textView)
+        checkBox = findViewById(R.id.checkBox)
+
+        
     }
+
+
+
+
+
 }
