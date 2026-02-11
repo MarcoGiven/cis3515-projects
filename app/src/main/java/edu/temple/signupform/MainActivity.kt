1df6eefd01ba385dc40cdf1b07c07420c4bbadc0
MarcoGiven <marcogiven15@gmail.com>
2026-02-11 14:14:03 -0500

    Added all views with constraints & initialized in code

--- app/src/main/java/edu/temple/signupform/MainActivity.kt
+++ app/src/main/java/edu/temple/signupform/MainActivity.kt
@@ -1,6 +1,10 @@
 package edu.temple.signupform
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val name = findViewById<EditText>(R.id.editTextText)
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val password = findViewById<EditText>(R.id.editTextTextPassword)
+        val passwordConfirm = findViewById<EditText>(R.id.editTextTextPasswordConfirm)
+        val save = findViewById<Button>(R.id.button)
+
+        val programs = arrayOf("Please Select Your Program", "Information Science", "Computer Science", "Math and CS", "Data Science", "Other")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, programs)
+        spinner.adapter = adapter
+
+
         
+
     }
 }
