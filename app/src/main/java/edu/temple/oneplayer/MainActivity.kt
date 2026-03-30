3203d92cf76adf0149ce2549a8fdb19095193e88
MarcoGiven <marcogiven15@gmail.com>
2026-03-29 21:53:42 -0400

    initial commit and made book data class

--- app/src/main/java/edu/temple/oneplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/oneplayer/MainActivity.kt
@@ -3,6 +3,12 @@ package edu.temple.oneplayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+data class Book(
+    val title: String,
+    val author: String
+)
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
