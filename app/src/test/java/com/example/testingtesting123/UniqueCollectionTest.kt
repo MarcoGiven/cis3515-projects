33358bf27663b09a75154eebf6046465a2ad112c
MarcoGiven <marcogiven15@gmail.com>
2026-04-09 12:35:42 -0400

    forgot to commit code from class :(

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -1,5 +1,6 @@
 package com.example.testingtesting123
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -16,13 +17,28 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        val size_og = collection.size()
+        var count = 0;
 
+        for(i in 1..10) {
+            collection.addItem(Item("item_$i"))
+            count += 1
+        }
+
+        assertEquals(size_og + count, collection.size())
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        collection.addItem(Item("ITEM1"))
+        collection.addItem(Item("item3"))
+        collection.addItem(Item("ITEM2"))
+
+        assertEquals(3, collection.size())
 
     }
 
